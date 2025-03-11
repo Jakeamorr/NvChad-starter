1499020c6f303425f1ba19f96af04705efc5d3dc
Jake Morris <jakeamorr@gmail.com>
2025-03-11 11:20:35 -0700

    forgot to remove extra symbols from git diff output when copying

--- lua/options.lua
+++ lua/options.lua
@@ -1,16 +1,16 @@
 require "nvchad.options"
 
 -- add yours here!
-+vim.api.nvim_create_autocmd({"BufWinLeave"}, {
-+  pattern = {"*.*"},
-+  desc = "save view (folds), when closing file",
-+  command = "mkview",
-+})
+vim.api.nvim_create_autocmd({"BufWinLeave"}, {
+  pattern = {"*.*"},
+  desc = "save view (folds), when closing file",
+  command = "mkview",
+})
 
-+vim.api.nvim_create_autocmd({"BufWinEnter"}, {
-+  pattern = {"*.*"},
-+  desc = "load view (folds), when opening file",
-+  command = "silent! loadview"
-+})
+vim.api.nvim_create_autocmd({"BufWinEnter"}, {
+  pattern = {"*.*"},
+  desc = "load view (folds), when opening file",
+  command = "silent! loadview"
+})
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
